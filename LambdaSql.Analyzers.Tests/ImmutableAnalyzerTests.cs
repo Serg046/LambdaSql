a2760139fd8a820674dcf8fc79bd4bc693a6536c
Serg046 <serg046@outlook.com>
2018-09-02 00:11:23 +0300

    Add roslyn ImmutableAnalyzer

---
+++ LambdaSql.Analyzers.Tests/ImmutableAnalyzerTests.cs
@@ -0,0 +1,59 @@
+using System;
+using LambdaSql.Analyzers.Tests.Verifiers;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Xunit;
+
+namespace LambdaSql.Analyzers.Tests
+{
+    public class ImmutableAnalyzerTests : DiagnosticVerifier
+    {
+        [Theory]
+        [InlineData("new SqlSelect<int>().Distinct()", "LambdaSql.SqlSelect<int>", "Distinct")]
+        [InlineData("new SqlSelect<int>().Distinct().Distinct(false)", "LambdaSql.SqlSelect<int>", "Distinct")]
+        public void InvalidExpression_DiagnosticIsReported(string expression, string type, string method)
+        {
+var test = $@"
+namespace LambdaSql.Analyzers.Tests
+{{
+    class TestType
+    {{
+        void TestMethod()
+        {{
+            {expression}
+        }}
+    }}
+}}";
+            var expected = new DiagnosticResult
+            {
+                Id = "LSql1000",
+                Message = String.Format(ImmutableAnalyzer.MESSAGE_FORMAT, type, method),
+                Severity = DiagnosticSeverity.Error,
+                Locations = new[] {new DiagnosticResultLocation(null, 0, 0)}
+            };
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        [Theory]
+        [InlineData("var qry = new SqlSelect<int>().Distinct()")]
+        [InlineData("var qry = new SqlSelect<int>().Distinct().Distinct(false)")]
+        public void ValidExpression_DiagnosticIsReported(string expression)
+        {
+            var test = $@"
+namespace LambdaSql.Analyzers.Tests
+{{
+    class TestType
+    {{
+        void TestMethod()
+        {{
+            {expression}
+        }}
+    }}
+}}";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new ImmutableAnalyzer();
+    }
+}
---
+++ LambdaSql.Analyzers/ImmutableAnalyzer.cs
@@ -0,0 +1,45 @@
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace LambdaSql.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ImmutableAnalyzer : DiagnosticAnalyzer
+    {
+        internal const string MESSAGE_FORMAT = "'{0}' is immutable and '{1}' will not have any effect on it. Consider using the return value from '{1}'.";
+
+        private static readonly DiagnosticDescriptor _rule = new DiagnosticDescriptor("LSql1000",
+            "Do not ignore values returned by methods on immutable objects.",
+            MESSAGE_FORMAT,
+            "Immutability",
+            DiagnosticSeverity.Error, isEnabledByDefault: true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(_rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterCompilationStartAction(compilationStartContext =>
+            {
+                var lambdaSqlAssembly = compilationStartContext.Compilation.GetTypeByMetadataName("LambdaSql.SqlSelect")?.ContainingAssembly;
+                compilationStartContext.RegisterSyntaxNodeAction(analysisContext =>
+                    Analyze(analysisContext, lambdaSqlAssembly), SyntaxKind.InvocationExpression);
+            });
+        }
+
+        private static void Analyze(SyntaxNodeAnalysisContext context, IAssemblySymbol lambdaSqlAssembly)
+        {
+            var invocation = (InvocationExpressionSyntax)context.Node;
+            var symbolInfo = context.SemanticModel.GetSymbolInfo(invocation, context.CancellationToken).Symbol;
+            if (symbolInfo != null && ReferenceEquals(symbolInfo.ContainingAssembly, lambdaSqlAssembly) && invocation.Parent.IsKind(SyntaxKind.ExpressionStatement))
+            {
+                var diagnostic = Diagnostic.Create(_rule, symbolInfo.Locations.Single(),
+                    symbolInfo.ContainingType.ToString(), symbolInfo.Name);
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
